537e659814b8f6133deabea266bb14b188e2455e
Ezgi Güven <IS96732@gm.isbank.com.tr>
2016-05-17 09:37:14 +0300

    hw2

---
+++ hw2/program.js
@@ -0,0 +1,20 @@
+var func = function() 
+ { 
+	var result = " ";
+ 	for (i = 1; i<=7; i++) 
+	{ 
+		for (j = 1; j<=i; j++) 
+		{
+			result += "#";
+			if(i==j)
+			{
+				result +="<br>";
+			}
+		}
+		
+	}
+	return result;
+ };	 
+ 
+
+ document.write(func()); 
