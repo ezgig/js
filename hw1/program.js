9fed57dbc12cb07c991a3698da472398a7d29e7e
Ezgi Güven <IS96732@gm.isbank.com.tr>
2016-05-17 18:39:15 +0300

    chessBoard

---
+++ hw1/program.js
@@ -0,0 +1,22 @@
+var func = function() 
+ { 
+    var result = "";
+	for( var i = 1; i <= 8; i++) 
+	{ 
+		for(j = 1; j <= 8; j++) 
+		{ 
+			if((i+j)%2 == 1 ) 
+			{ 
+				 result += "O"; 
+			 }else 
+			{ 
+				 result += "X"; 
+			 } 
+		}
+		result += "<br>";
+	} 
+	 return result;
+ };	 
+ 
+
+ document.write(func()); 
